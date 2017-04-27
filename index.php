fc00844aacb82c8c00ac8c5b02ae02d6516f2f42
chenpz <smoothsea@yeah.net>
2017-04-27 17:40:52 +0800

    修改普通循环为libevent事件驱动

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ class Trh
     private $port = "";
     private $protocal = "";
     public $count = 1;
+    public static $event = null;
+    public static $masterPid = null;
 
     public function __construct($address, $port, $protocal)
     {
@@ -15,39 +17,75 @@ class Trh
 
     public function run()
     {
+        $this->registerEvent();
         $this->start();
     }
 
     private function start()
     {
-        $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-        socket_bind($socket, $this->address, $this->port);
+        $context = stream_context_create();
+        $socket = stream_socket_server("tcp://{$this->address}:{$this->port}", $errno, $errmsg,
+            STREAM_SERVER_BIND | STREAM_SERVER_LISTEN, $context);
+        stream_set_blocking($socket, 0);
 
+        if (!$socket) {
+            echo "error:{$errmsg}";
+            exit;
+        }
 
         for ($i=0; $i<$this->count; $i++) {
             $pid = pcntl_fork();
 
             if ($pid == 0) {
-                while (1) {
-                    socket_listen($socket);
-                    $connection = socket_accept($socket);
-
-                    $request = socket_read($connection, 8192);
-                    $httpHeader = "HTTP/1.1 200 OK\r\n" .
-                        "Server:self\r\n" .
-                        "Content-Type:text/html\r\n\r\n";
-                    socket_write($connection, $httpHeader . "hello world");
-                    socket_close($connection);
-                }
+                $event = event_new();
+                event_set($event, $socket, EV_READ | EV_PERSIST, [$this, "acceptConnection"]);
+                event_base_set($event, self::$event);
+                event_add($event);
+                event_base_loop(self::$event);
+            } else {
+                self::$masterPid = getmypid();
             }
 
         }
 
-        pcntl_wait($status);
+        if (self::$masterPid == getmypid()) {
+            while (1) {
+                pcntl_wait($status);
+            }
+        }
+
+    }
+
+    private function acceptConnection($connection)
+    {
+        $connection = @stream_socket_accept($connection, 0, $remoteAddress);
+
+        // Thundering herd.
+        if (!$connection) {
+            return ;
+        }
+
+        $request = fread($connection, 8093);
+        echo $request;
+        $httpHeader = "HTTP/1.1 200 OK\r\n" .
+            "Server:self\r\n" .
+            "Content-Type:text/html\r\n\r\n";
+        $r = fwrite($connection, $httpHeader."this is server ".getmypid(), 9999);
+        fclose($connection);
+    }
+
+    private function registerEvent()
+    {
+        if (extension_loaded("libevent") ) {
+            self::$event = event_base_new();
+            return true;
+        }
+
+        exit("please load libevent extension");
     }
 }
 
-$trh = new Trh("192.168.2.233", "8001", "http");
+$trh = new Trh("192.168.9.60", "8001", "http");
 $trh->count = 4;
 $trh->run();
 
